6a97deb532159aeb358d41fbed574b921a8f1c5d
fsrice <harlanlambert@gmail.com>
2024-03-04 23:07:37 +0800

    Set

--- Set/demo.js
+++ Set/demo.js
@@ -1,5 +1,6 @@
 const Set = require('./index')
 
 const set = new Set()
+// test
 set.add(1)
 console.log(set.add(1), set.values())
--- Set/index.js
+++ Set/index.js
@@ -24,6 +24,43 @@ function Set () {
   Set.prototype.values = function () {
     return Object.keys(this.data)
   }
+  // union of two sets
+  Set.prototype.union = function (set) {
+    const res = new Set()
+    for (const key in this.data) {
+      res.add(key)
+    }
+    for (const key in set.values) {
+      res.add(key)
+    }
+    return res
+  }
+  // intersection of two sets
+  Set.prototype.intersection = function (set) {
+    const res = new Set()
+    for (const key in this.data) {
+      if (set.has(key)) res.add(key)
+    }
+    return res
+  }
+  // difference of two sets
+  Set.prototype.difference = function (set) {
+    const res = new Set()
+    for (const key in this.data) {
+      if (!set.has(key)) res.add(key)
+    }
+    for (const key in set.values) {
+      if (!this.has(key)) res.add(key)
+    }
+    return res
+  }
+  // determine whether the current set is a subset of a set
+  Set.prototype.subset = function (set) {
+    for (const key in this.data) {
+      if (!set.has(key)) return false
+    }
+    return true
+  }
 }
 
 module.exports = Set
